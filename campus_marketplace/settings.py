a32874ec44cd714435cc2b78d33609acf1837aaf
Vansh <shivagupta37205@gmail.com>
2026-04-23 10:32:57 +0530

    update settings and fix deployment issues

--- campus_marketplace/settings.py
+++ campus_marketplace/settings.py
@@ -128,4 +128,4 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
