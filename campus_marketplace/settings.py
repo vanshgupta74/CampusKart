2ec491cae3efab68fb8045e8bdf3f32739a064d4
Vansh <shivagupta37205@gmail.com>
2026-07-31 00:11:09 +0530

    fix: session engine and email config

--- campus_marketplace/settings.py
+++ campus_marketplace/settings.py
@@ -172,3 +172,6 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = 'CampusKart <campuskart.noreply@gmail.com>'
+
+# Memory optimize karo
+SESSION_ENGINE = 'django.contrib.sessions.backends.db'  # DB mein sessions rakho
