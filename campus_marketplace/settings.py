ee689b926ad1b2720b67b3f22e6baecb94b7457c
Vansh <shivagupta37205@gmail.com>
2026-07-31 01:02:24 +0530

    fix: email credentials fallback added

--- campus_marketplace/settings.py
+++ campus_marketplace/settings.py
@@ -167,9 +167,11 @@ SUPERUSER_PASSWORD = os.environ.get('SUPERUSER_PASSWORD', '')
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp-relay.brevo.com'
-EMAIL_PORT = 465
-EMAIL_USE_TLS = False
-EMAIL_USE_SSL = True
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 DEFAULT_FROM_EMAIL = 'CampusKart <shivagupta37205@gmail.com>'
 EMAIL_TIMEOUT = 10
 
